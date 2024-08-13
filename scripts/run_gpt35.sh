4ad4e6b49aac9a3951e86043e062b714cc3735a9
anonymous <116339475+codeanonymous928@users.noreply.github.com>
2024-08-13 11:00:02 +0800

    Update run_gpt35.sh

--- scripts/run_gpt35.sh
+++ scripts/run_gpt35.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=8 python run.py \
     --data_path=data/question_samples.json \
-    --response_path=response/response.json \
+    --response_path=responses/responses.json \
     --model_name=gpt-3.5-turbo-16k-0613 \
     --mkt=zh-CN \
     --build_type=llm
