23cb3ef382e9fdc1b34d7542ec1cbc417bb9fc5b
anonymous <116339475+codeanonymous928@users.noreply.github.com>
2024-08-13 11:01:19 +0800

    Update run_glm4.sh

--- scripts/run_glm4.sh
+++ scripts/run_glm4.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=9 python run.py \
     --data_path=data/question_samples.json \
     --response_path=responses/responses.json \
-    --model_name=glm-4 \
+    --model_name=glm-4-0520 \
     --mkt=zh-CN \
     --build_type=llm
