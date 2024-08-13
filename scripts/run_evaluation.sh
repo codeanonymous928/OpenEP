91eb341b476f1164b0b6f4954fd6b7e1a05e62ee
anonymous <116339475+codeanonymous928@users.noreply.github.com>
2024-08-13 11:01:37 +0800

    Update run_evaluation.sh

--- scripts/run_evaluation.sh
+++ scripts/run_evaluation.sh
@@ -1,5 +1,5 @@
 python evaluation.py \
-    --predictions_path=response/response.json \
+    --predictions_path=responses/responses.json \
     --questions_path=data/question_samples.json \
     --ground_truth_path=data/answer_samples.json \
     --model_name=gpt-4-turbo
