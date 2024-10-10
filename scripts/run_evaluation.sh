024960a01c6f4ba34038194eecd4be3a694de2e9
anonymous <116339475+codeanonymous928@users.noreply.github.com>
2024-10-10 22:06:42 +0800

    Delete scripts/run_evaluation.sh

--- scripts/run_evaluation.sh
+++
@@ -1,5 +0,0 @@
-python evaluation.py \
-    --predictions_path=responses/responses.json \
-    --questions_path=data/question_samples.json \
-    --ground_truth_path=data/answer_samples.json \
-    --model_name=gpt-4-turbo
